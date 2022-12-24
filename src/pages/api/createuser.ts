fa2542ac870465721e51b03fb9a2df409a5b060a
KrishnaSatti <satti.krishna.r@gmail.com>
2022-12-23 20:50:28 -0500

    create user works no encryption

---
+++ src/pages/api/createuser.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from "next";
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://admin:donisking@cluster0.w2jjljq.mongodb.net/?retryWrites=true&w=majority";
+
+async function monogoProducts(request) {
+  const client = new MongoClient(uri);
+  let data = {};
+  try {
+    const database = client.db("products");
+    const collection = database.collection("food");
+    const query = { username: request.username };
+    const result = await collection.findOne(query);
+    if (result != undefined) {
+      data = { acknowledged: false, insertedId: "null" };
+    } else {
+      const doc = {
+        username: request.username,
+        password: request.password,
+        name: request.name,
+      };
+      const status = await collection.insertOne(doc);
+      data = status;
+    }
+  } finally {
+    await client.close();
+  }
+
+  return data;
+}
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  console.log(req.body.username);
+  const data = await monogoProducts(req.body);
+  res.json(data);
+}
+
+//https://localhost:3000/api/createuser
